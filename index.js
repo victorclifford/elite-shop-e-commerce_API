d0b46a4806deaa54cb1583f3d97ef11d83469ce0
victor clifford <victorclifford29@gmail.com>
2022-08-19 08:20:14 +0100

    fixed something

--- index.js
+++ index.js
@@ -9,15 +9,7 @@ const app = express();
 
 // middlewares
 // Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin, credentials: true }));
 app.use(express.json());
 app.use("/api/products", productRoutes);
 
