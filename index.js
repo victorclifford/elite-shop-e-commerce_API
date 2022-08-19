c76a8fac36a41c25ec23b6c6c6a0be06962302ab
victor clifford <victorclifford29@gmail.com>
2022-08-19 07:58:36 +0100

    fixed something

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const origin = ["http://localhost:3000"];
 const app = express();
 
 // middlewares
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(cors({ credentials: true, origin }));
 app.use(express.json());
 app.use("/api/products", productRoutes);
